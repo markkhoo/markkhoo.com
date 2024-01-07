197e8267c250133f85af96c6d747c0e9ad720b94
Mark Khoo <markdkhoo@gmail.com>
2024-01-06 23:49:26 -0800

    UPDATE implement zIndex order

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
   import ResizableWindow from '$lib/components/resizableWindow.svelte';
+
+  let zIndex = 0;
+
+  function bringToFront() {
+    zIndex += 1;
+    return zIndex;
+  }
 </script>
 
 <main>
-  <ResizableWindow>box</ResizableWindow>
-  <ResizableWindow initPosTop={50} initPosLeft={50} zIndex={-2} backgroundColor={'#dddddd'}>another box lmao</ResizableWindow>
+  <ResizableWindow zIndex={bringToFront}>box</ResizableWindow>
+  <ResizableWindow zIndex={bringToFront} initPosTop={50} initPosLeft={50} backgroundColor={'#dddddd'}>another box lmao</ResizableWindow>
 </main>
 
 <style>
