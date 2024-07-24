ee23ce9bf004b7904829e0463f4984fbf0f5eede
Mark Khoo <markdkhoo@gmail.com>
2024-07-24 13:50:34 -0700

    USE pnpm (#3)
    * USE pnpm
    
    * UPDATE deps sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-  import ResizableWindow from '$lib/components/resizableWindow.svelte';
+	import ResizableWindow from '$lib/components/resizableWindow.svelte';
 
-  let zIndex = 0;
+	let zIndex = 0;
 
-  function bringToFront() {
-    zIndex += 1;
-    return zIndex;
-  }
+	function bringToFront() {
+		zIndex += 1;
+		return zIndex;
+	}
 </script>
 
 <main>
-  <ResizableWindow zIndex={bringToFront}>box</ResizableWindow>
-  <ResizableWindow zIndex={bringToFront} initPosTop={50} initPosLeft={50} backgroundColor={'#dddddd'}>another box lmao</ResizableWindow>
+	<ResizableWindow zIndex={bringToFront}>box</ResizableWindow>
+	<ResizableWindow
+		zIndex={bringToFront}
+		initPosTop={50}
+		initPosLeft={50}
+		backgroundColor={'#dddddd'}>another box lmao</ResizableWindow
+	>
 </main>
 
 <style>
-  main {
-    display: block;
-    position: relative;
-  }
-
-  :global(body) {
-    /* 'user agent stylesheet' adds margin to body sometimes */
-    margin: 0;
-  }
+	main {
+		display: block;
+		position: relative;
+	}
 </style>
